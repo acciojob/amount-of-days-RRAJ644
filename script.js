447175109424cac3f3621bec0ea46fb375b5ac34
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-10-27 23:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//your JS code here. If required.
-functiondaysOfAYear (year) {
-	if((year % 4 ===0 && year %100 !==0) || year % 400 ===0 ) return 366
-	else return 365
+function daysOfAYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; // Leap year
+    } else {
+        return 365; // Non-leap year
+    }
 }
